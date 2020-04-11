d7dd45f937c1363e3900a84965e54da1747f9192
Tomas <tomvargasd@gmail.com>
2020-04-10 19:14:12 -0500

    chatbot using chatterbot with python

---
+++ chatbot.py
@@ -0,0 +1,16 @@
+from chatterbot import ChatBot
+bot=ChatBot(
+	"AI",
+	storage_adapter = "chatterbot.storage.SQLStorageAdapter",
+	logic_adapter = [
+		"chatterbot.logic.BestMatch",
+		"chatterbot.logic.TimeLogicAdapter",
+		"chatterbot.logic.MathematicalEvaluation"
+	],
+	input_adapter = "chatterbot.input.TerminalAdapter",
+	output_adapter = "chatterbot.output.TerminalAdapter",
+	database = "./database.db"
+)
+
+while True:
+	print ('bot: ',bot.get_response(None))
