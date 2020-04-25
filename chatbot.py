a5789e188811f87e845602b18b9e94ebbb80af6a
Tomas <tomvargasd@gmail.com>
2020-04-24 21:50:25 -0500

    start to incorporate GUI

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,5 @@
 from chatterbot import ChatBot
+from tkinter import *
 bot=ChatBot(
 	"AI",
 	storage_adapter = "chatterbot.storage.SQLStorageAdapter",
@@ -11,6 +12,27 @@ bot=ChatBot(
 	output_adapter = "chatterbot.output.TerminalAdapter",
 	database = "./database.accdb")
 
-while True:
-	r=bot.get_response(input(">> "))
-	print ('bot: ',r)
+root=Tk()
+root.geometry("400x600")	
+root.resizable(False, False)
+root.title("BOT")
+
+usr=""
+r="No entiendo lo que dices"
+
+def res():
+	re=bot.get_response(usr)
+	r.set(re)
+	
+Label(root,text="CHAT-BOT").pack()
+Label(root,text="").pack()
+Label(root,text="Message").pack()
+Label(root,text="").pack()
+Entry(root,textvariable=usr).pack()
+Label(root,text="").pack()
+Entry(root,textvariable=r,state="disabled").pack()
+Label(root,text="").pack()
+Button(root,text="Enviar", command=res).pack()
+
+
+root.mainloop()
