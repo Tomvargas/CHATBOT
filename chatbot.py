edff6354d4de0e8b49fa6a0a023c32ac4a5e7350
Tomas <tomvargasd@gmail.com>
2020-04-29 22:33:06 -0500

    note: fix the error

--- chatbot.py
+++ chatbot.py
@@ -12,27 +12,38 @@ bot=ChatBot(
 	output_adapter = "chatterbot.output.TerminalAdapter",
 	database = "./database.accdb")
 
-root=Tk()
-root.geometry("400x600")	
-root.resizable(False, False)
-root.title("BOT")
+def main():
+	root=Tk()
+	root.geometry("400x300")	
+	root.resizable(False, False)
+	root.title("BOT")
 
-usr=""
-r="No entiendo lo que dices"
+	usr=StringVar() #distancia de surcos
+	usr.set(0)
+	r=StringVar()
+	r.set(0)
+    
 
-def res():
-	re=bot.get_response(usr)
-	r.set(re)
-	
-Label(root,text="CHAT-BOT").pack()
-Label(root,text="").pack()
-Label(root,text="Message").pack()
-Label(root,text="").pack()
-Entry(root,textvariable=usr).pack()
-Label(root,text="").pack()
-Entry(root,textvariable=r,state="disabled").pack()
-Label(root,text="").pack()
-Button(root,text="Enviar", command=res).pack()
+	def res():
+		Us=usr.get()
+		re=bot.get_response("holaaa")
+		r.set(re)
+
+		
+	Label(root,text="CHAT-BOT").pack()
+	Label(root,text="").pack()
+	Label(root,text="Message").pack()
+	Label(root,text="").pack()
+	Entry(root,textvariable=usr).pack()
+	Label(root,text="").pack()
+	Entry(root,textvariable=r,state="disabled").pack()
+	Label(root,text="").pack()
+	Button(root,text="Enviar", command=res).pack()
 
 
-root.mainloop()
+
+
+
+	root.mainloop()
+
+main()
